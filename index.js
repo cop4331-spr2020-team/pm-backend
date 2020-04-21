49e8c2b0bedab164e1b32ea965380d56eeb364c4
Marlon Calvo <marloncalv98@gmail.com>
2020-04-21 04:05:03 -0400

    fix cors issue

--- index.js
+++ index.js
@@ -11,10 +11,26 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan('combined'));
+
+const allowedOrigins = ['http://localhost:3000',
+  'http://localhost:8080', 'http://www.parkingmanagerapp.com'];
+
+app.use(cors({
+  origin: function(origin, callback) {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+}));
 
 initMongo.connect()
     .then(() => {
